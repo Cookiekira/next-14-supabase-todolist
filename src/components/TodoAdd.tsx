523a0d4408494f031e373b698a92a60e393e9c88
Cookiekira <oddc2000@outlool.com>
2023-11-18 12:31:54 +1100

    chore: update todo add component

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -2,13 +2,19 @@
 
 import { Button, Input } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import { addTodo } from '@/app/(main)/actions'
 
 export function TodoAdd() {
   const [newTask, setNewTask] = useState('')
   const router = useRouter()
+
+  const handleAddTodo = useCallback(async () => {
+    await addTodo(newTask)
+    setNewTask('')
+    router.refresh()
+  }, [newTask, router])
 
   return (
     <>
@@ -17,16 +23,21 @@ export function TodoAdd() {
         placeholder='What needs to be done?'
         radius='sm'
         size='sm'
+        value={newTask}
         onChange={(e) => {
           setNewTask(e.target.value)
+        }}
+        onKeyUp={async (e) => {
+          if (e.key === 'Enter') {
+            await handleAddTodo()
+          }
         }}
       />
       <Button
         radius='sm'
         size='lg'
         onClick={async () => {
-          await addTodo(newTask)
-          router.refresh()
+          await handleAddTodo()
         }}
       >
         Add

bf20f0717573b00e8818053ff1e5a2bfefa14733
Cookiekira <oddc2000@outlool.com>
2023-11-28 12:41:14 +1100

    chore: Update dependencies and add missing styles

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,7 @@ export default async function TodoHome() {
   const initialTodos = await getTodos()
 
   return (
-    <main className='max-w-2xl min-h-[50vh] mx-auto flex flex-col items-center justify-center'>
+    <main className='max-w-2xl min-h-[50vh] mx-auto flex flex-col items-center justify-center px-4'>
       <h1 className='text-[4rem] font-bold w-full mb-8'>Todo List.</h1>
 
       <TodoList initialTodos={initialTodos.data ?? []} />

29439f7cf44c2c4559d0353741325c66a10dd7d0
Cookiekira <oddc2000@outlool.com>
2024-01-09 22:35:57 +1100

    chore: Refactor login page UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import { cookies, headers } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { createClient, createClientWithCookies } from '@/supabase/server'
 
 import { SubmitButton } from './_component/SubmitButton'
@@ -85,51 +86,61 @@ export default async function Login({
   return (
     <>
       <main className='py-5 rounded-lg  flex-1 flex flex-col w-full px-8 sm:max-w-md gap-2 mx-auto  absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
-        <form action={signInWithGithub}>
-          <SubmitButton className='w-full flex items-center justify-center border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-6  gap-2 bg-slate-100 hover:bg-slate-200 '>
-            <span className='i-ph-github-logo' />
-            Sign in with Github
-          </SubmitButton>
-        </form>
+        <Card>
+          <CardHeader>
+            <CardTitle>Sign In</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form action={signInWithGithub}>
+              <SubmitButton className='w-full flex items-center justify-center border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-6  gap-2 bg-slate-100 hover:bg-slate-200 '>
+                <span className='i-ph-github-logo' />
+                Sign in with Github
+              </SubmitButton>
+            </form>
 
-        <form
-          className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground  '
-          action={signIn}
-        >
-          <label className='text-md' htmlFor='email'>
-            Email
-          </label>
-          <input
-            className='rounded-md px-4 py-2 bg-inherit border mb-6'
-            name='email'
-            placeholder='you@example.com'
-            required
-          />
-          <label className='text-md' htmlFor='password'>
-            Password
-          </label>
-          <input
-            className='rounded-md px-4 py-2 bg-inherit border mb-6'
-            type='password'
-            name='password'
-            placeholder='••••••••'
-            required
-          />
-          <SubmitButton className='bg-green-500 hover:bg-green-500/90 rounded-md px-4 py-2 text-foreground mb-2'>
-            Sign In
-          </SubmitButton>
-          <SubmitButton
-            formAction={signUp}
-            className='border border-foreground/20 rounded-md px-4 py-2 bg-secondary text-secondary-foreground
+            <form
+              className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground  '
+              action={signIn}
+            >
+              <label className='text-md' htmlFor='email'>
+                Email
+              </label>
+              <input
+                className='rounded-md px-4 py-2 bg-inherit border mb-6'
+                name='email'
+                placeholder='you@example.com'
+                required
+              />
+              <label className='text-md' htmlFor='password'>
+                Password
+              </label>
+              <input
+                className='rounded-md px-4 py-2 bg-inherit border mb-6'
+                type='password'
+                name='password'
+                placeholder='••••••••'
+                required
+              />
+              <SubmitButton className='bg-green-500 hover:bg-green-500/90 rounded-md px-4 py-2 text-foreground mb-2'>
+                Sign In
+              </SubmitButton>
+              <SubmitButton
+                formAction={signUp}
+                className='border border-foreground/20 rounded-md px-4 py-2 bg-secondary text-secondary-foreground
              hover:bg-secondary/50
             mb-2'
-          >
-            Sign Up
-          </SubmitButton>
-          {searchParams?.message && (
-            <Toast message={searchParams.message} type={searchParams.type} />
-          )}
-        </form>
+              >
+                Sign Up
+              </SubmitButton>
+              {searchParams?.message && (
+                <Toast
+                  message={searchParams.message}
+                  type={searchParams.type}
+                />
+              )}
+            </form>
+          </CardContent>
+        </Card>
       </main>
     </>
   )
